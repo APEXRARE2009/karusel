1a870e917d3686736f0b8aeb14e1a467227fac39
2009 <83235862+APEXRARE2009@users.noreply.github.com>
2024-04-14 19:18:58 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,31 +1,60 @@
 let beforeBtn = document.querySelector(".content__before-btn");
 let afterBtn = document.querySelector(".content__after-btn");
 let img = document.querySelector(".content__hero-img");
+let afterBlock = document.querySelector(".main__content-after-block");
 
 let links = ["https://images.wallpapersden.com/image/download/3d-abstract-colorful-surface-line_a2lqZWmUmZqaraWkpJRqZmdlrWdtbWU.jpg", "https://images.wallpapersden.com/image/download/warframe-hd-2022_bWxrbGWUmZqaraWkpJRqZmdlrWdtbWU.jpg", "https://images.wallpapersden.com/image/download/denji-chainsaw-man-cool-8k_bWxsaWWUmZqaraWkpJRsa21lrWloZ2U.jpg", "https://images.wallpapersden.com/image/download/2020-audi-rs6-gto-style_bGtnaWeUmZqaraWkpJRqZmdlrWdtbWU.jpg"];
 
 let i = 0;
 
+function addIndexer() {
+    let index = document.createElement("div");
+    index.classList.add("main__content-after-block-indexer");
+    afterBlock.appendChild(index);
+}
+
+for (let num = 0; num < links.length; num++) {
+    addIndexer()
+}
+
+let afterBlockIndex = Array.from(document.querySelectorAll(".main__content-after-block-indexer"));
+
+let idNum = 0;
+
+afterBlockIndex.map((indexer) => {
+    indexer.id = idNum;
+    idNum++
+})
+
+function toggleIndexer() {
+    afterBlockIndex.map((indexer) => {
+        indexer.classList.remove("main__content-after-block-indexer-active")
+        if (indexer.id == i) {
+            indexer.classList.add("main__content-after-block-indexer-active")
+        }
+    })
+}
 
 function toggleLink() {
-	console.log(i);
-	img.src = links[i];
+    console.log(i);
+    img.src = links[i];
+    toggleIndexer()
 }
 
 toggleLink()
 
 beforeBtn.addEventListener("click", () => {
-	i--
-	if (i < 0) {
-		i = links.length - 1
-	}
-	toggleLink()
+    i--
+    if (i < 0) {
+        i = links.length - 1
+    }
+    toggleLink()
 });
 
 afterBtn.addEventListener("click", () => {
-	i++
-	if (i > links.length - 1) {
-		i = 0
-	}
-	toggleLink()
+    i++
+    if (i > links.length - 1) {
+        i = 0
+    }
+    toggleLink()
 });
