756181a2b4d3a9f5681727c417e2042be8cebd35
2009 <83235862+APEXRARE2009@users.noreply.github.com>
2024-04-18 17:28:50 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ let afterBtn = document.querySelector(".content__after-btn");
 let img = document.querySelector(".content__hero-img");
 let afterBlock = document.querySelector(".main__content-after-block");
 
-let links = ["https://images.wallpapersden.com/image/download/3d-abstract-colorful-surface-line_a2lqZWmUmZqaraWkpJRqZmdlrWdtbWU.jpg", "https://images.wallpapersden.com/image/download/warframe-hd-2022_bWxrbGWUmZqaraWkpJRqZmdlrWdtbWU.jpg", "https://images.wallpapersden.com/image/download/denji-chainsaw-man-cool-8k_bWxsaWWUmZqaraWkpJRsa21lrWloZ2U.jpg", "https://images.wallpapersden.com/image/download/2020-audi-rs6-gto-style_bGtnaWeUmZqaraWkpJRqZmdlrWdtbWU.jpg"];
+let linksUrl = ["https://images.wallpapersden.com/image/download/3d-abstract-colorful-surface-line_a2lqZWmUmZqaraWkpJRqZmdlrWdtbWU.jpg", "https://images.wallpapersden.com/image/download/warframe-hd-2022_bWxrbGWUmZqaraWkpJRqZmdlrWdtbWU.jpg", "https://images.wallpapersden.com/image/download/denji-chainsaw-man-cool-8k_bWxsaWWUmZqaraWkpJRsa21lrWloZ2U.jpg", "https://images.wallpapersden.com/image/download/2020-audi-rs6-gto-style_bGtnaWeUmZqaraWkpJRqZmdlrWdtbWU.jpg"];
+let links = ["img/wallpaper-1.jpg", "img/wallpaper-2.jpg", "img/wallpaper-3.jpg", "img/wallpaper-4.jpg"];
 
 let i = 0;
 
